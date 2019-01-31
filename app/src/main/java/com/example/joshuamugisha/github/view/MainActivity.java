8ead3ba8fc7abf23e31c7d4b113ad96c10f2ec04
Joshua Mugisha <joshuamugisha@MacBook-Pro-90.local>
2019-01-31 12:52:21 +0300

    feat(UI/UX Revamp): UI/UX Revamp - add UI/UX Revamp
    [Delivers #156037248]

--- app/src/main/java/com/example/joshuamugisha/github/view/MainActivity.java
+++ app/src/main/java/com/example/joshuamugisha/github/view/MainActivity.java
@@ -1,15 +1,29 @@
 package com.example.joshuamugisha.github.view;
 
+
 import android.graphics.Color;
 import android.os.Bundle;
 import android.os.Parcelable;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+
+import android.content.res.Configuration;
+import android.os.Bundle;
+import android.os.Parcelable;
+import android.support.v4.widget.SwipeRefreshLayout;
+
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.GridLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
+
 import android.widget.TextView;
+
+import android.widget.ProgressBar;
+import android.widget.Toast;
+
 
 import com.example.joshuamugisha.github.Application.MyApplication;
 import com.example.joshuamugisha.github.R;
@@ -22,24 +36,27 @@ import java.util.List;
 
 public class MainActivity extends AppCompatActivity implements UsersView, NetworkReceiver.NetworkReceiverListener{
     private RecyclerView recyclerView;
+
     private FloatingActionButton floatingActionButton;
     private GithubPresenter githubPresenter;
-    private LinearLayoutManager mLayoutManager;
+    private SwipeRefreshLayout swipeContainer;
+    private ProgressBar progressBar;
+    private GridLayoutManager mLayoutManager;
     public final static String LIST_STATE_KEY = "recycler_list_state";
     Parcelable listState;
-
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         recyclerView = findViewById(R.id.userRecyclerView);
+        swipeContainer = findViewById(R.id.swipeContainer);
+        progressBar = findViewById(R.id.indeterminateBar);
         floatingActionButton = findViewById(R.id.fab);
-
         checkConnection();
-
         githubPresenter = new GithubPresenter(this);
         githubPresenter.getGithubUsers();
+
 
         floatingActionButton.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -50,14 +67,65 @@ public class MainActivity extends AppCompatActivity implements UsersView, Networ
             }
         });
 
-        mLayoutManager =new LinearLayoutManager(this);
+
+        if (this.getResources().getConfiguration().orientation == Configuration.ORIENTATION_PORTRAIT) {
+            mLayoutManager = new GridLayoutManager(this, 2);
+        } else {
+            mLayoutManager = new GridLayoutManager(this, 3);
+        }
+
+
         recyclerView.setLayoutManager(mLayoutManager);
 
+        swipeContainer.setOnRefreshListener(new SwipeRefreshLayout.OnRefreshListener() {
+            @Override
+            public void onRefresh() {
+                githubPresenter.getGithubUsers();
+            }
+        });
+        swipeContainer.setColorSchemeResources(android.R.color.holo_blue_bright,
+                android.R.color.holo_green_light,
+                android.R.color.holo_orange_light,
+                android.R.color.holo_red_light);
+    }
+
+    @Override
+    public void onConfigurationChanged(Configuration newConfig) {
+        super.onConfigurationChanged(newConfig);
+
+        // Checks the orientation of the screen
+        if (newConfig.orientation == Configuration.ORIENTATION_LANDSCAPE) {
+            mLayoutManager = new GridLayoutManager(this, 3);
+            Toast.makeText(this, "landscape", Toast.LENGTH_SHORT).show();
+        } else if (newConfig.orientation == Configuration.ORIENTATION_PORTRAIT) {
+            mLayoutManager = new GridLayoutManager(this, 2);
+        }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the actionbar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.menu_refresh:
+                swipeContainer.setRefreshing(true);
+                githubPresenter.getGithubUsers();
+                return true;
+        }
+        return super.onOptionsItemSelected(item);
 
     }
     @Override
     public void getGithubUsers(List<GithubUsers>githubUsers) {
         recyclerView.setAdapter(new GithubAdapter(this, githubUsers));
+        swipeContainer.setRefreshing(false);
+        progressBar.setVisibility(View.GONE);
+
     }
 
     protected void onSaveInstanceState(Bundle state) {
